66888f3afb4ba2b24c9c5969cce48c1cd458bdeb
Hi-Im-Jony <jccicai@yahoo.ie>
2021-07-01 23:30:37 +0100

    created simple nav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="nav">
-    <v-app-bar color="black" fixed app>
+    <v-app-bar id="navbar" color="blue " fixed app dense>
       <h1>Roofing and Guttering</h1>
+      <v-spacer />
+      <div id="deets-container">
+        <a class="deet" href="mailto:bbuilding@gmail.com">
+          bbuilding@gmail.com
+        </a>
+        <a class="deet" href="tel:+353873794038">
+          +353 87 379 4038
+        </a>
+      </div>
     </v-app-bar>
   </div>
 </template>
@@ -13,7 +22,16 @@ export default {
 </script>
 
 <style scoped>
-#nav {
-  /* */
+#navbar {
+  color: rgb(235, 37, 37) !important;
+  background: rgb(41, 9, 158) !important;
+}
+.deet {
+  color: rgb(202, 21, 21);
+  padding: 10px;
+  font-size: 1.1rem;
+}
+.deet:hover {
+  color: rgb(235, 37, 37);
 }
 </style>

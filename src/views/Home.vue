fe0808adbc832706b515fc867e26b117a717add3
Hi-Im-Jony <jccicai@yahoo.ie>
2021-07-02 00:01:12 +0100

    developed the homepage a bit more

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>
+  <div id="homepage">
     <roof-banner />
-    <h1>hi</h1>
+    <p id="intro-text">
+      MJ Roofing Contractors offers the highest standard of roofing installation
+      and roofing repair to clients in Dublin, Kildare, Wicklow and Meath.
+
+      <br /><br />
+
+      Our team of Dublin roofing experts have over 25 years of experience and
+      complete every job to the very highest of regulatory standards.
+
+      <br /><br />
+
+      From pitched roofs to flat roofs, we have the expertise and experience to
+      complete everything from small roofing repairs to complete roofing
+      installations.
+
+      <br /><br />
+
+      We specialise in flat roof installations and flat roof repairs on home
+      extensions. We can refit old, poorly laid flat roofs or install a flat
+      roof on a new extension. Whatever the roofing requirement, clients can
+      rest assured that their roofing service will be completed to the high MJ
+      Roofing Contractors standard of quality and craftsmanship.
+
+      <br /><br />
+
+      All our roofing services come with a 25-year guarantee and are available
+      with a free no-obligation quote.
+    </p>
+    <mini-banner />
   </div>
 </template>
 
 <script>
+import MiniBanner from "../components/mini-banner.vue";
 import roofBanner from "../components/roof-banner.vue";
 
 export default {
@@ -13,6 +42,21 @@ export default {
 
   components: {
     roofBanner,
+    MiniBanner,
   },
 };
 </script>
+
+<style scoped>
+#homepage {
+  min-height: 100vh;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+#intro-text {
+  width: 50%;
+  margin: 30px 0 30px 0;
+}
+</style>

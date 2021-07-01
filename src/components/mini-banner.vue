8e5980e853bb91485f0f2f9ef2fb194540a87acf
Hi-Im-Jony <jccicai@yahoo.ie>
2021-07-02 00:21:44 +0100

    created mini banner

---
+++ src/components/mini-banner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="mini-banner">
+    <div id="text-container">
+      <p id="text">
+        For a free no-obligation quote on your next flat roofing repair or
+        pitched roof repair, get in contact with MJ Roofing Contractors today.
+      </p>
+      <phone-btn />
+    </div>
+    <div id="image-container">
+      <img id="roofer" src="../assets/roofer-2.jpg" alt="" />
+    </div>
+  </div>
+</template>
+<script>
+import phoneBtn from "./phone-btn.vue";
+export default {
+  components: { phoneBtn },
+  name: "mini-banner",
+};
+</script>
+
+<style scoped>
+#mini-banner {
+  background: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+#text-container {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+#text {
+  width: 55%;
+}
+#image-container {
+  width: 50%;
+}
+#roofer {
+  width: 100%;
+}
+</style>

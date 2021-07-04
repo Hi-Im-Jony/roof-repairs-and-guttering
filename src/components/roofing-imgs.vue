e9e0a54aa1aafdec936250697c39f14c6ef18b10
Hi-Im-Jony <jccicai@yahoo.ie>
2021-07-04 10:55:33 +0100

    getting somewhere with the gallery

--- src/components/roofing-imgs.vue
+++ src/components/roofing-imgs.vue
@@ -1,60 +1,62 @@
 <template>
-  <div id="imgs-container">
-    <div class="img-container">
+  <div class="grid">
+    <div class="grid-sizer"></div>
+    <div class="grid-item">
       <img src="../assets/roofs/roof1.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof2.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof3.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof4.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof5.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof6.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof7.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof8.jpeg" alt="" />
     </div>
-    <div class="img-container">
+    <div class="grid-item">
       <img src="../assets/roofs/roof9.jpeg" alt="" />
     </div>
   </div>
 </template>
 
 <script>
+import Masonry from "masonry-layout";
+window.onload = () => {
+  const grid = document.querySelector(".grid");
+  const masonry = new Masonry(grid, {
+    itemSelector: ".grid-item",
+    fitWidth: true,
+  });
+};
 export default {
   name: "roofingImgs",
 };
 </script>
 
 <style scoped>
-#imgs-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+.grid {
+  width: 100%;
+  padding: 0 !important;
+  margin: 0 !important;
 }
-.img-container {
-  width: 25vw;
-  height: 25vw;
-  max-width: 300px;
-  max-height: 300px;
-  margin: 5px;
-  border-width: 1px;
+.grid-item {
+  max-width: 30%;
+  min-width: 100px;
 }
-img {
-  object-fit: contain;
-  width: inherit;
-  height: inherit;
-  max-width: inherit;
-  max-height: inherit;
+.grid-item img {
+  padding: 5px;
+  width: 100%;
 }
 </style>
